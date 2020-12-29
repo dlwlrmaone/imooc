49590c039aaf8cd197dfa2b528bda36e971cea24
张宇 <zhangyu01@sinosoft.com.cn>
2020-12-29 16:41:30 +0800

    用户注册service层开发

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,5 +1,7 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
@@ -14,6 +16,7 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
+    //RequestParam一般接受简单类型的属性，也可以接受对象类型
     public IMOOCJSONResult usernameIsExist(@RequestParam String username){
 
         //此处使用apache.commons.lang3包的StringUtils工具类进行非空校验
@@ -22,5 +25,24 @@ public class PassportController {
         }
         boolean isExist = userService.queryUsernameIsExist(username);
         return isExist == true ? IMOOCJSONResult.errorMsg("用户名已存在") : IMOOCJSONResult.ok();
+    }
+
+    @PostMapping("/register")
+    //RequestBody常用于接收json数据
+    public IMOOCJSONResult register(@RequestBody UserBO userBO){
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPassword = userBO.getConfirmPassword();
+
+        //校验用户名和密码
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password) || StringUtils.isBlank(confirmPassword)){
+            return IMOOCJSONResult.errorMsg("用户名或密码不能为空");
+        }
+        boolean isExist = userService.queryUsernameIsExist(username);
+        return isExist == true ? IMOOCJSONResult.errorMsg("用户名已存在") : IMOOCJSONResult.ok();
+
+        //实现注册
+
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,22 +1,29 @@
 package com.imooc.service.impl;
 
+import com.imooc.enumclass.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.DateUtil;
 import com.imooc.utils.MD5Utils;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
 
     private static final String USER_FACE_URL = "http://122.152.205.72:88/group1/M00/00/05/CpooxFw_8_qAllFXAAAclhVPdSg994.png";
 
@@ -41,7 +48,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public Users createUser(UserBO userBO) {
 
+        String userId = sid.nextShort();
+
         Users users = new Users();
+        users.setId(userId);
         users.setUsername(userBO.getUsername());
         try {
             users.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
@@ -54,8 +64,13 @@ public class UserServiceImpl implements UserService {
         users.setFace(USER_FACE_URL);
         //默认生日
         users.setBirthday(DateUtil.stringToDate("1900-01-01");
-        users.setSex(1);
+        //默认性别为保密，使用枚举类来表示
+        users.setSex(Sex.secret.type);
+        users.setCreatedTime(new Date());
+        users.setUpdatedTime(new Date());
 
-        return null;
+        usersMapper.insert(users)
+
+        return users;
     }
 }
